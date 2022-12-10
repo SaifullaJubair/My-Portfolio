e1af22b905a16df8bb6bfcf866a4d5d8100bdc48
SaifullaJubair <saifullajubair6@gmail.com>
2022-12-11 00:26:11 +0600

    npm package install

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1 className='text-5xl'>Hello World</h1>
       <h2>Nice</h2>
     </div>
   );
