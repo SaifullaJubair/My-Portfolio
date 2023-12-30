a383ef907be1732e1870476410963b7314355f92
SaifullaJubair <saifullajubair6@gmail.com>
2023-12-30 22:34:17 +0600

    farebd link update

--- src/components/Projects/Project/FareBd.js
+++ src/components/Projects/Project/FareBd.js
@@ -79,26 +79,26 @@ const FareBd = () => {
         </p>
       </div>
       <span className="inline-flex -space-x-px overflow-hidden rounded-md border bg-white shadow-sm my-6">
-        <button
+        <a
           className="inline-block px-4 py-2  font-medium text-gray-600  hover:bg-info transition hover:scale-110 focus:relative"
           href="https://farebd-9.web.app/"
         >
           Live Site
-        </button>
+        </a>
 
-        <button
+        <a
           className="inline-block px-4 py-2  font-medium text-gray-600 hover:bg-info transition hover:scale-110 focus:relative"
           href="https://github.com/SaifullaJubair/FareBD"
         >
           GitHub Client
-        </button>
+        </a>
 
-        <button
+        <a
           className="inline-block px-4 py-2  font-medium text-gray-600 hover:bg-info transition hover:scale-110 focus:relative"
           href="https://github.com/SaifullaJubair/Server-FareBD"
         >
           GutHub Server
-        </button>
+        </a>
       </span>
     </div>
   );
