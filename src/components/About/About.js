3a114a662ecd7f46c70e5c36a8edb8e39ccd4c0c
SaifullaJubair <saifullajubair6@gmail.com>
2024-01-06 17:32:19 +0600

    about js update

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,16 +13,64 @@ const About = () => {
               <div className="h-1 ml-auto duration-300 origin-left transform bg-deep-purple-accent-400 scale-x-30 group-hover:scale-x-100" />
             </h2>
             <p className="lg:max-w-3xl mx-auto">
-              Hi! I'm Saifulla Jubair, a MERN Stack developer who learned and
-              was certified by Programming Hero. I have done some full-stack
-              projects. I'm an expert with React Js, JavaScript, ES6, REST API,
-              Firebase, Node Js, MongoDB, Stripe Payment Integration, React
-              Query, React Router, React Bootstrap, Bootstrap, HTML5, CSS3,
-              Tailwind CSS, Daisy UI and Material UI to create various features
-              for client websites. I am fast learner at technical term. I like
-              to learn new technologies and apply to my projects for make my
-              projects up to date. With my knowledge and practice I am confident
-              & excited to work with your organization.
+              <div className="mt-1">
+                Hey there! I'm MD Saifulla Jubair, a passionate MERN Stack
+                Developer hailing from the vibrant city of Khulna, Bangladesh.
+                My journey into the world of web development started with a
+                profound fascination for creating seamless, user-friendly
+                digital experiences.
+              </div>{" "}
+              <br />
+              <div className="mt-1">
+                As a graduate with a keen interest in Physics, I embarked on a
+                path of continuous learning and growth. Alongside my academic
+                pursuits at Government Sundarban Adarsha College, I delved into
+                the realm of web development. My thirst for knowledge led me to
+                enroll in the Complete Web Development Course at Programming
+                Hero, where I honed my skills and earned a certification in
+                2022.
+              </div>{" "}
+              <br />
+              <div className="mt-1">
+                The capstone of my educational journey marked the completion of
+                the course, equipping me with expertise in a diverse range of
+                technologies. From mastering React and Node.js to navigating the
+                intricacies of MongoDB and Firebase, I've embraced the MERN
+                stack with enthusiasm.
+              </div>{" "}
+              <br />
+              <div className="mt-1">
+                One of my standout achievements is the development of
+                Shovon’sGallery, an e-commerce platform showcasing artificial
+                ornaments and antique jewelry. This project not only reflects my
+                technical prowess in ReactJS, Tailwind, Node.js, and more but
+                also underscores my commitment to user-centric design, featuring
+                elements like SSLCommcez for secure payments and Firebase for
+                seamless user authentication.
+              </div>{" "}
+              <br />
+              <div className="mt-1">
+                I also took the reins as a leader in the FareBD project, an
+                innovative house rental and selling app. Guiding a dynamic team,
+                I oversaw the implementation of user type verification, seamless
+                buyer-seller interactions, and integration with technologies
+                like Stripe and TanStack Query.
+              </div>{" "}
+              <br />
+              <div className="mt-1">
+                Beyond coding, my proficiency extends to tools like GitHub,
+                VSCode, and design applications like Photoshop and Figma. Fluent
+                in both Bangla and English, I bring a holistic skill set and a
+                creative edge to every project.
+              </div>
+              <br />
+              <div className="mt-1">
+                In a nutshell, I'm on a continuous journey of growth,
+                exploration, and creation in the ever-evolving landscape of web
+                development. Excited to connect, collaborate, and bring digital
+                visions to life!
+              </div>{" "}
+              <br />
             </p>
           </div>
         </div>
